825b28b3da86090120efea13134df4618d9169d1
Cristian Ciupitu <cristian.ciupitu@yahoo.com>
2011-11-18 08:55:19 +0200

    Update for the latest php module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,3 @@
 class prestashop {
     include php, php::gd, php::mysql, php::mcrypt
-
-    # workaround for bug https://github.com/duritong/puppet-mysql/issues/2
-    if $mysql_exists == 'true' {
-        Class["mysql::server::service"]
-            -> Class["httpd::service"]
-            -> Class["prestashop"]
-    } else {
-        include mysql::server::install
-    }
 }
